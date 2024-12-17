3d56caa770943339a5222de76654b6794c0494c4
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-12-17 17:51:18 +0200

    main.cpp changes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "ClusteringForbiddenEdges.h"
 #include "ClusteringCycleDetection.h"
 #include "ClusteringHybrid.h"
+#include "GreedyDirectedGraphGrowing.h"
+#include "UndirectedFix.h"
 #include <iostream>
 #include <chrono>
 
@@ -15,14 +17,25 @@ int main() {
 //    Graph graph = readDotFile("/home/panagiotis/code/dag-partitioning/partition-graph-1.dot",
 //                              "/home/panagiotis/code/dag-partitioning/node-mappings.txt");
 
-    ClusteringForbiddenEdges clusteringForb(graph);
-    clusteringForb.runClustering();
+//    ClusteringForbiddenEdges clusteringForb(graph);
+//    clusteringForb.runClustering();
+//
+//    ClusteringCycleDetection clusteringCyc(graph);
+//    clusteringCyc.runClustering();
+//
+//    ClusteringHybrid clusteringHyb(graph);
+//    clusteringHyb.runClustering();
 
-    ClusteringCycleDetection clusteringCyc(graph);
-    clusteringCyc.runClustering();
+    GreedyDirectedGraphGrowing GDGG(graph, 1.2 * ((double) graph.totalWeight / 2.0), 1.0);
 
-    ClusteringHybrid clusteringHyb(graph);
-    clusteringHyb.runClustering();
+    auto pair = GDGG.run();
+//    for (int i = 0; i < pair.first.size(); i++) {
+//        std::cout << i << " " << pair.first[i] << "\n";
+//    }
+//    std::cout << pair.second << "\n";
+
+    UndirectedFix UF(graph, 1.2 * ((double) graph.totalWeight / 2.0), 1.0);
+    UF.getUndirectedPartition();
 
     return 0;
 }
