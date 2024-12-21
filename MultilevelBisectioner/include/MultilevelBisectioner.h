5d62eb5b16b766cf285bfac1b18394e69b0506a2
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-12-21 19:37:57 +0200

    Implement Multi level bisectioner that performs coarsening, initial bisection and iterative uncoarsening + refinement (dummy refinement for now)

---
+++ MultilevelBisectioner/include/MultilevelBisectioner.h
@@ -0,0 +1,53 @@
+//
+// Created by panagiotis on 21/12/2024.
+//
+
+#ifndef DAG_PARTITIONING_MULTILEVELBISECTIONER_H
+#define DAG_PARTITIONING_MULTILEVELBISECTIONER_H
+
+
+#include <stack>
+#include "Graph.h"
+
+enum class ClusteringMethod {
+    FORB,
+    CYC,
+    HYB
+};
+
+enum class BisectionMethod {
+    GGG,
+    UNDIRMETIS,
+    UNDIRSCOTCH,
+    UNDIRBOTH
+};
+
+class MultilevelBisectioner {
+private:
+    Graph workingGraph;
+    ClusteringMethod clusteringMethod;
+    uint64_t maxClusteringRounds;
+    uint64_t minClusteringVertices;
+    BisectionMethod bisectionMethod;
+    double imbalanceRatio;
+public:
+    explicit MultilevelBisectioner(Graph graph, ClusteringMethod clusteringMethod,
+                                   uint64_t maxClusteringRounds,
+                                   uint64_t minClusteringVertices,
+                                   BisectionMethod bisectionMethod,
+                                   double imbalanceRatio);
+
+    [[nodiscard]] std::stack<std::pair<Graph, std::vector<uint64_t>>> runClustering() const;
+
+    [[nodiscard]] std::pair<std::vector<bool>, uint64_t> runBisection(const Graph &graph) const;
+
+    static void projectBisection(std::pair<std::vector<bool>, uint64_t> &bisectionInfo,
+                                 const std::vector<uint64_t> &mapping);
+
+    static void dummyRefinement(const Graph &graph, std::pair<std::vector<bool>, uint64_t> &bisectionInfo);
+
+    [[nodiscard]] std::pair<std::vector<bool>, uint64_t> run() const;
+};
+
+
+#endif //DAG_PARTITIONING_MULTILEVELBISECTIONER_H
---
+++ MultilevelBisectioner/src/MultilevelBisectioner.cpp
@@ -0,0 +1,106 @@
+//
+// Created by panagiotis on 21/12/2024.
+//
+
+#include "MultilevelBisectioner.h"
+#include "ClusteringForbiddenEdges.h"
+#include "ClusteringCycleDetection.h"
+#include "ClusteringHybrid.h"
+#include "GreedyDirectedGraphGrowing.h"
+#include "UndirectedFix.h"
+#include <utility>
+
+MultilevelBisectioner::MultilevelBisectioner(Graph graph, ClusteringMethod clusteringMethod,
+                                             uint64_t maxClusteringRounds,
+                                             uint64_t minClusteringVertices,
+                                             BisectionMethod bisectionMethod,
+                                             double imbalanceRatio) :
+        workingGraph(std::move(graph)), clusteringMethod(clusteringMethod), maxClusteringRounds(maxClusteringRounds),
+        minClusteringVertices(minClusteringVertices), bisectionMethod(bisectionMethod),
+        imbalanceRatio(imbalanceRatio) {}
+
+std::stack<std::pair<Graph, std::vector<uint64_t>>> MultilevelBisectioner::runClustering() const {
+    std::unique_ptr<Clustering> clustering;
+    switch (clusteringMethod) {
+        case ClusteringMethod::FORB:
+            clustering = std::make_unique<ClusteringForbiddenEdges>(workingGraph, maxClusteringRounds,
+                                                                    minClusteringVertices);
+            break;
+        case ClusteringMethod::CYC:
+            clustering = std::make_unique<ClusteringCycleDetection>(workingGraph, maxClusteringRounds,
+                                                                    minClusteringVertices);
+            break;
+        case ClusteringMethod::HYB:
+            clustering = std::make_unique<ClusteringHybrid>(workingGraph, maxClusteringRounds, minClusteringVertices);
+            break;
+        default:
+            throw std::invalid_argument("Unknown clustering type");
+    }
+
+    std::stack<std::pair<Graph, std::vector<uint64_t>>> intermediateClusters = clustering->run();
+    return intermediateClusters;
+}
+
+std::pair<std::vector<bool>, uint64_t> MultilevelBisectioner::runBisection(const Graph &graph) const {
+    double lowerBoundPartWeight = 1.0;
+    double upperBoundPartWeight = imbalanceRatio * ((double) workingGraph.totalWeight / 2.0);
+    std::unique_ptr<Bisection> bisection;
+    switch (bisectionMethod) {
+        case BisectionMethod::GGG:
+            bisection = std::make_unique<GreedyDirectedGraphGrowing>(graph, upperBoundPartWeight,
+                                                                     lowerBoundPartWeight);
+            break;
+        case BisectionMethod::UNDIRMETIS:
+            bisection = std::make_unique<UndirectedFix>(graph, upperBoundPartWeight,
+                                                        lowerBoundPartWeight, true, false);
+            break;
+        case BisectionMethod::UNDIRSCOTCH:
+            bisection = std::make_unique<UndirectedFix>(graph, upperBoundPartWeight,
+                                                        lowerBoundPartWeight, false, true);
+            break;
+        case BisectionMethod::UNDIRBOTH:
+            bisection = std::make_unique<UndirectedFix>(graph, upperBoundPartWeight,
+                                                        lowerBoundPartWeight, true, true);
+            break;
+        default:
+            throw std::invalid_argument("Unknown bisection type");
+    }
+
+    std::pair<std::vector<bool>, uint64_t> bisectionInfo = bisection->run();
+    return bisectionInfo;
+}
+
+void MultilevelBisectioner::projectBisection(std::pair<std::vector<bool>, uint64_t> &bisectionInfo,
+                                             const std::vector<uint64_t> &mapping) {
+    std::vector<bool> newBisection(mapping.size());
+    for (uint64_t i = 0; i < mapping.size(); ++i) {
+        newBisection[i] = bisectionInfo.first[mapping[i]];
+    }
+    bisectionInfo.first = newBisection;
+}
+
+void MultilevelBisectioner::dummyRefinement(const Graph &graph,
+                                            std::pair<std::vector<bool>, uint64_t> &bisectionInfo) {
+}
+
+std::pair<std::vector<bool>, uint64_t> MultilevelBisectioner::run() const {
+    std::stack<std::pair<Graph, std::vector<uint64_t>>> intermediateClusters = runClustering();
+
+    auto [coarsestGraph, coarsestMapping] = intermediateClusters.top();
+    std::pair<std::vector<bool>, uint64_t> bisectionInfo = runBisection(coarsestGraph);
+    dummyRefinement(coarsestGraph, bisectionInfo);
+    intermediateClusters.pop();
+
+    auto &currentMapping = coarsestMapping;
+    while (!intermediateClusters.empty()) {
+        projectBisection(bisectionInfo, currentMapping);
+        auto [intermediateGraph, intermediateMapping] = intermediateClusters.top();
+        currentMapping = intermediateMapping;
+        dummyRefinement(intermediateGraph, bisectionInfo);
+        intermediateClusters.pop();
+    }
+
+    projectBisection(bisectionInfo, currentMapping);
+    dummyRefinement(workingGraph, bisectionInfo);
+    return bisectionInfo;
+}
