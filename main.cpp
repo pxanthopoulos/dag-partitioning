e9fdc6b5d6c1de24cb20a8ba4c2d39996b33b65b
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-12-22 19:18:56 +0200

    Extend tests

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
 #include "Graph.h"
-#include "ClusteringForbiddenEdges.h"
-#include "ClusteringCycleDetection.h"
-#include "ClusteringHybrid.h"
-#include "GreedyDirectedGraphGrowing.h"
-#include "UndirectedFix.h"
+#include "MultilevelBisectioner.h"
 #include <iostream>
 #include <chrono>
 
@@ -12,30 +8,21 @@ int main() {
 //                              "/home/panagiotis/code/dag-partitioning/node-mappings.txt");
     Graph graph = readDotFile("/home/panagiotis/code/dag-partitioning/test/dag.dot",
                               "/home/panagiotis/code/dag-partitioning/node-mappings.txt");
-//    Graph graph = readDotFile("/home/panagiotis/code/dag-partitioning/before.dot",
-//                              "/home/panagiotis/code/dag-partitioning/node-mappings.txt");
-//    Graph graph = readDotFile("/home/panagiotis/code/dag-partitioning/partition-graph-1.dot",
-//                              "/home/panagiotis/code/dag-partitioning/node-mappings.txt");
 
-//    ClusteringForbiddenEdges clusteringForb(graph);
-//    clusteringForb.runClustering();
-//
-//    ClusteringCycleDetection clusteringCyc(graph);
-//    clusteringCyc.runClustering();
-//
-//    ClusteringHybrid clusteringHyb(graph);
-//    clusteringHyb.runClustering();
+    MultilevelBisectioner mb_forb_undir(graph, ClusteringMethod::FORB, 20, 1, BisectionMethod::UNDIRBOTH, 1.2);
+    MultilevelBisectioner mb_cyc_undir(graph, ClusteringMethod::CYC, 20, 1, BisectionMethod::UNDIRBOTH, 1.2);
+    MultilevelBisectioner mb_hyb_undir(graph, ClusteringMethod::HYB, 20, 1, BisectionMethod::UNDIRBOTH, 1.2);
 
-    GreedyDirectedGraphGrowing GDGG(graph, 1.2 * ((double) graph.totalWeight / 2.0), 1.0);
+    MultilevelBisectioner mb_forb_ggg(graph, ClusteringMethod::FORB, 20, 1, BisectionMethod::GGG, 1.2);
+    MultilevelBisectioner mb_cyc_ggg(graph, ClusteringMethod::CYC, 20, 1, BisectionMethod::GGG, 1.2);
+    MultilevelBisectioner mb_hyb_ggg(graph, ClusteringMethod::HYB, 20, 1, BisectionMethod::GGG, 1.2);
 
-    auto pair = GDGG.run();
-//    for (int i = 0; i < pair.first.size(); i++) {
-//        std::cout << i << " " << pair.first[i] << "\n";
-//    }
-//    std::cout << pair.second << "\n";
+    auto vec = mb_forb_undir.run();
+    vec = mb_cyc_undir.run();
+    vec = mb_hyb_undir.run();
 
-    UndirectedFix UF(graph, 1.2 * ((double) graph.totalWeight / 2.0), 1.0);
-    UF.getUndirectedPartition();
-
+    vec = mb_forb_ggg.run();
+    vec = mb_cyc_ggg.run();
+    vec = mb_hyb_ggg.run();
     return 0;
 }
--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for ratio in 100 150 200 300 500 750 1000; do
-    for value in 20 50 100 200 500 1000 2000 5000; do
-        for i in {1..200}; do
+for ratio in 100 120 140 160 180 200 250; do
+    for value in 20 30 40 50 60 70 80 90 100; do
+        for i in {1..1000}; do
             echo "Run $i with value $value,ratio $ratio"
             ./rand-dag $value $ratio && \
             /home/panagiotis/code/dag-partitioning/cmake-build-debug/dag_partitioning || \
