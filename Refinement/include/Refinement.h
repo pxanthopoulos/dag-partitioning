e697869ef5488ba1169ea2a99255727dfb510b4b
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-12-31 02:34:58 +0200

    Add helper methods related to partition size balancing

--- Refinement/include/Refinement.h
+++ Refinement/include/Refinement.h
@@ -13,12 +13,18 @@ protected:
     const Graph &workingGraph;
     std::vector<bool> &initialBisectionInfo;
     uint64_t maxNumberOfPasses;
+    double upperBoundPartWeight, lowerBoundPartWeight;
 
     [[nodiscard]] bool checkValidBisection() const;
 
+    [[nodiscard]] std::pair<uint64_t, uint64_t> calculatePartSizes() const;
+
+    [[nodiscard]] bool checkBalance(uint64_t maxNodeWeight) const;
+
     virtual bool onePassRefinement() = 0;
 
-    Refinement(const Graph &graph, std::vector<bool> &initialBisectionInfo, uint64_t maxNumberOfPasses);
+    Refinement(const Graph &graph, std::vector<bool> &initialBisectionInfo, uint64_t maxNumberOfPasses,
+               double upperBoundPartWeight, double lowerBoundPartWeight);
 
 public:
     virtual ~Refinement() = default;
--- Refinement/src/Refinement.cpp
+++ Refinement/src/Refinement.cpp
@@ -4,8 +4,10 @@
 
 #include "Refinement.h"
 
-Refinement::Refinement(const Graph &graph, std::vector<bool> &initialBisectionInfo, uint64_t maxNumberOfPasses)
-        : workingGraph(graph), initialBisectionInfo(initialBisectionInfo), maxNumberOfPasses(maxNumberOfPasses) {}
+Refinement::Refinement(const Graph &graph, std::vector<bool> &initialBisectionInfo, uint64_t maxNumberOfPasses,
+                       double upperBoundPartWeight, double lowerBoundPartWeight)
+        : workingGraph(graph), initialBisectionInfo(initialBisectionInfo), maxNumberOfPasses(maxNumberOfPasses),
+          upperBoundPartWeight(upperBoundPartWeight), lowerBoundPartWeight(lowerBoundPartWeight) {}
 
 bool Refinement::checkValidBisection() const {
     for (uint64_t i = 0; i < workingGraph.size; ++i) {
@@ -17,10 +19,30 @@ bool Refinement::checkValidBisection() const {
     return true;
 }
 
+std::pair<uint64_t, uint64_t> Refinement::calculatePartSizes() const {
+    uint64_t sizeV0 = 0, sizeV1 = 0;
+    for (uint64_t i = 0; i < initialBisectionInfo.size(); ++i) {
+        if (!initialBisectionInfo[i]) sizeV0 += workingGraph.nodeWeights[i];
+        else sizeV1 += workingGraph.nodeWeights[i];
+    }
+    return {sizeV0, sizeV1};
+}
+
+bool Refinement::checkBalance(uint64_t maxNodeWeight) const {
+    auto [sizeV0, sizeV1] = calculatePartSizes();
+    if ((double) sizeV0 < lowerBoundPartWeight - (double) maxNodeWeight ||
+        (double) sizeV0 > upperBoundPartWeight + (double) maxNodeWeight ||
+        (double) sizeV1 < lowerBoundPartWeight - (double) maxNodeWeight ||
+        (double) sizeV1 > upperBoundPartWeight + (double) maxNodeWeight)
+        return false;
+    return true;
+}
+
 void Refinement::run() {
     uint64_t countPasses = 0;
     while (countPasses < maxNumberOfPasses) {
         assert(checkValidBisection() && "Bisection is invalid, it has edge from V1 to V0");
         if (!onePassRefinement()) break;
     }
+    assert(checkBalance(workingGraph.maxNodeWeight()) && "Resulting partition is unbalanced");
 }
