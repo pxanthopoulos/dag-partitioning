07f9e773c653a82f066e42d36d6d4c3c569fc394
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-12-31 15:12:54 +0200

    Extend tests to support time-outs

--- test/test.sh
+++ test/test.sh
@@ -5,9 +5,13 @@ for ratio in 100 120 140 160 180 200 250; do
         for i in {1..1000}; do
             echo "Run $i with value $value,ratio $ratio"
             ./rand-dag $value $ratio && \
-            /home/panagiotis/code/dag-partitioning/cmake-build-debug/dag_partitioning || \
+            timeout --foreground 10s /home/panagiotis/code/dag-partitioning/cmake-build-debug/dag_partitioning || \
             {
+                ret=$?
                 echo "Error: dag-partitioning failed for value $value, ratio $ratio on iteration $i"
+                if [ $ret -eq 124 ]; then
+                    echo "TIMED OUT"
+                fi
                 exit 1
             }
         done
